875af6592d89b8b710dc0b1070191a5932da235b
Abhishek0880 <98387256+Abhishek0880@users.noreply.github.com>
2024-07-05 18:00:42 +0530

    Update urls.py

--- Nimap_Project-main/nimap_project/urls.py
+++ Nimap_Project-main/nimap_project/urls.py
@@ -8,14 +8,8 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'client', ClientViewSet)
-# router.register(r'ClientList', ClientList)
-# router.register(r'ClientDetail', ClientDetail)
 router.register(r'project',ProjectViewSet)
-#router.register(r'ProjectList', ProjectList)
-# router.register(r'ProjectDetail', ProjectDetail)
 
-# Setup automatic URL routing
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
